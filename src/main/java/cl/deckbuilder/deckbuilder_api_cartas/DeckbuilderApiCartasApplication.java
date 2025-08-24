fa3243e74379e72099acf35409cd24f4861c1354
amendezllaupe <amendezllaupe@gmail.com>
2025-08-24 19:48:37 -0400

    Init

---
+++ src/main/java/cl/deckbuilder/deckbuilder_api_cartas/DeckbuilderApiCartasApplication.java
@@ -0,0 +1,13 @@
+package cl.deckbuilder.deckbuilder_api_cartas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeckbuilderApiCartasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeckbuilderApiCartasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/deckbuilder/deckbuilder_api_cartas/DeckbuilderApiCartasApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.deckbuilder.deckbuilder_api_cartas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeckbuilderApiCartasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
